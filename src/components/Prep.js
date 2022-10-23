7bc0b4eb615aaac422ec47561cb9ea845f008698
hafiz <hafizn24@gmail.com>
2022-10-23 19:03:19 +0800

    Adjust Size for Prep

--- src/components/Prep.js
+++ src/components/Prep.js
@@ -5,14 +5,14 @@ import pdf from './pdf/Airbnb Preprocessing.pdf'
 function Prep() {
   return (
     <>
-        <Box sx={{mt:0, mb:2}}>
+        <Box sx={{mt:0, mb:2, p:2}}>
             <Typography variant='h5' align="center"><b>Data Preparation</b></Typography>
         </Box>
         <Box
           align="center"
             sx={{
                 width: '100%',
-                height: '1000',
+                height: 'auto',
                 backgroundColor: 'secondary.main',
             }}
         >
