0bfddeecffc7bbda406d3698dc7623076ee75f6c
hafiz <hafizn24@gmail.com>
2022-10-23 19:00:02 +0800

    Adjust The Style for Hero

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -28,106 +28,107 @@ function Hero() {
                         alt="logo"
                     />
                 </Box>
-                <Typography variant="h5" sx={{mt:2}}><b>Data Preparation</b> and <b>Visualization</b></Typography>
+                <Typography variant="h5" sx={{m:2}}><b>Data Preparation</b> and <b>Visualization</b></Typography>
             </Grid>
         </Box>
         <Box
             sx={{
-                mt:2,
+                p:2,
                 width: '100%',
                 height: 'auto',
                 backgroundColor: 'primary.light',
             }}
         >
-
-            <Box
+            <Grid
+                container
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
             >
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
+                <Box
+                    sx={{
+                        width:{
+                            lg:'50%'
+                        }
+                    }}
+                >
+                    <CardMedia 
+                        component="img"
+                        width="auto"
+                        image={room}
+                        alt="fyp"
+                    />
+                </Box>
+                <Box
+                    sx={{
+                        p:2
+                    }}
                 >
                     <Box
                         sx={{
-                            width:450,
-                            height:{
-                                sx:200,
-                                lg:300
-                            },
-                            mx:2,
-                            mt:2
+                            lg:'50%'
                         }}
                     >
-                        <CardMedia 
-                            component="img"
-                            width="auto"
-                            image={room}
-                            alt="room"
-                        />  
-                    </Box>
-                    <Box
-                        sx={{
-                            width:450,
-                            height:300,
-                            mx:2,
-                            mt:2
-                        }}
-                    >
-                        <Typography variant="subtitle1" color="primary.dark" sx={{fontWeight: 'bold'}} align="center">
-                            Airbnb, Inc is an American company that operates an online marketplace for lodging, 
-                            primarily homestays for vacation rentals and tourism activities. 
-                            Based in San Francisco, California, the platform is accessible via a website and mobile app. 
-                            Airbnb does not own any listed properties; instead, it profits by receiving a commission from each booking. 
-                            The company was founded in 2008. Airbnb is a shortened version of its original name, AirBedandBreakfast.com.
+                        <Typography variant="subtitle1" color="primary.dark" sx={{fontWeight: 'bold'}}>
+                            Airbnb, Inc is an American company that operates an online marketplace for lodging, <br/>
+                            primarily homestays for vacation rentals and tourism activities. <br/>
+                            Based in San Francisco, California, the platform is accessible via a website and mobile app. <br/>
+                            Airbnb does not own any listed properties; <br />
+                            instead, it profits by receiving a commission from each booking. <br/>
+                            The company was founded in 2008. <br/>
+                            Airbnb is a shortened version of its original name, AirBedandBreakfast.com.
                         </Typography>
                     </Box>
-                </Grid>
-                <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    alignItems="center"
+                </Box>
+            </Grid>
+        </Box>
+        <Box
+            sx={{
+                p:2,
+                width: '100%',
+                height: 'auto',
+                backgroundColor: 'primary.light',
+            }}
+        >
+            <Grid
+                container
+                direction="row-reverse"
+                justifyContent="space-around"
+                alignItems="center"
+            >
+                <Box
+                    sx={{
+                        width:{
+                            lg:'50%'
+                        }
+                    }}
+                >
+                    <CardMedia 
+                        component="img"
+                        width="auto"
+                        image={table}
+                        alt="fyp"
+                    />
+                </Box>
+                <Box
+                    sx={{
+                        p:2
+                    }}
                 >
                     <Box
                         sx={{
-                            width:450,
-                            height:{
-                                sx:200,
-                                lg:300
-                            },
-                            mx:2,
-                            mt:2
+                            lg:'50%'
                         }}
                     >
-                        <Typography variant="subtitle1" color="primary.dark" sx={{fontWeight: 'bold'}} align='center'>
-                            The following Airbnb activity is included in this New York dataset:
-                            Listings, including full descriptions and average review score Reviews, 
-                            including unique id for each reviewer and detailed comments Calendar, 
+                        <Typography variant="subtitle1" color="primary.dark" sx={{fontWeight: 'bold'}}>
+                            The following Airbnb activity is included in this New York dataset: <br/>
+                            Listings, including full descriptions and average review score Reviews, <br/>
+                            including unique id for each reviewer and detailed comments Calendar, <br/>
                             including listing id and the price and availability for that day
-                        </Typography> 
+                        </Typography>
                     </Box>
-                    <Box
-                        sx={{
-                            width:450,
-                            height:{
-                                sx:200,
-                                lg:300
-                            },
-                            mx:2,
-                            my:2
-                        }}
-                    >
-                        <CardMedia 
-                            component="img"
-                            width="auto"
-                            image={table}
-                            alt="table"
-                        />  
-                    </Box>
-                </Grid>
-            </Box>
-
+                </Box>
+            </Grid>
         </Box>
     </>
   )
